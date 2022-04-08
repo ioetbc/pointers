9f394a80c8a7b7fc5364b974f14e363dba9510a4
ioetbc <ioetbc@gmail.com>
2022-04-08 10:33:39 +0100

    stuff

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.use(express.static(__dirname + "/public"));
-
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
@@ -16,10 +14,6 @@ io.on("connection", (socket) => {
     socket.broadcast.emit("chat message", msg);
   });
 });
-
-// io.on("connection", (socket) => {
-//   socket.broadcast.emit("hi");
-// });
 
 server.listen(3000, () => {
   console.log("listening on *:3000");
